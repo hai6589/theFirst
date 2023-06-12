816cd829756d817d61f96273f34157bf36923975
hai6589 <114660856+hai6589@users.noreply.github.com>
2023-06-12 13:18:12 +0700

    upd

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
 let btn1 = document.getElementById('btn-back');
 btn1.onclick = function () {
-    window.location = '../theFirst/js/form.js';
+    window.location = '../theFirst/html/main.html';
 }
